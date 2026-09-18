8dab0cba93621b688d6c6e2db6bc866f2bdf75e3
Harizo <hei.harizo.2@gmail.com>
2026-09-18 14:51:20 +0300

    fix: exo2 and exo3

--- exo2.js
+++ exo2.js
@@ -7,11 +7,14 @@ function Echanger(tab, element1, element2){
 console.log(Echanger([0, 1, 0], 1,2));
 
 function Ranger(tab){
-    let result = [];
-    for (let element of tab){
-        if(element == 0){ result.unshift(element);}else{result.push(element)}
+    let indexTemp = 0;
+    for (let i = 1; i < tab.length; i++) {
+        if(tab[indexTemp] == 1 && tab[i] == 0){ 
+            Echanger(tab, indexTemp, i);
+        }
+        indexTemp++;
     }
-    return result;
+    return tab;
 }
 console.log(Ranger([0, 1, 0, 0, 1, 0]));
 
--- exo3.js
+++ exo3.js
@@ -1,15 +1,19 @@
 import NombreUn from "./exo2.js";
+
 function triLignes(matrice){
-    let temp = 0;
-    let result = [];
+    let indexTemp = 0;
     for (let i = 1; i < matrice.length; i++) {
-        if(NombreUn(matrice[i]) < NombreUn(matrice[temp])){
-            result.push(matrice[temp]);
-        }else{result.unshift(matrice[temp]);}
-        temp++;
+        let valueTemp = 0;
+        if(NombreUn(matrice[i]) < NombreUn(matrice[indexTemp])){
+            valueTemp = matrice[indexTemp];
+            matrice[indexTemp] = matrice[i];
+            matrice[i] = valueTemp;
+        }
+        console.log(matrice);
+        
+        indexTemp++;
     }
-    result.unshift(matrice[temp]);
-    return result;
+    return matrice;
 }
 console.log(triLignes([ 
   [0, 0, 1, 1], 
